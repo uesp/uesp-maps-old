35a24a57133c910f22f2af6b83d8ecb0aaeda45d
Dave Humphrey <dave@uesp.net>
2013-11-28 21:15:36 -0500

    add script to more easily update the JS versions in all map files

---
+++ updatejsver.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+#
+# Changes the JS version number in all the map HTML files.
+# by Dave Humphrey (dave@uesp.net)
+
+INPUTVER="$1"
+
+if [ -z $INPUTVER ]; then
+	NOW=`date +'%Y-%m-%d'`
+	INPUTVER="$NOW"
+fi
+
+echo "Changing map JS version to '$INPUTVER'..."
+
+sed -i "s/\(\.js?\)\(.*\)\(\"><\/script>\)/\1$INPUTVER\3/g" ./dbmap/dbmap.html
+sed -i "s/\(\.js?\)\(.*\)\(\"><\/script>\)/\1$INPUTVER\3/g" ./obmap/obmap.html
+sed -i "s/\(\.js?\)\(.*\)\(\"><\/script>\)/\1$INPUTVER\3/g" ./mwmap/mwmap.html
+sed -i "s/\(\.js?\)\(.*\)\(\"><\/script>\)/\1$INPUTVER\3/g" ./simap/simap.html
+sed -i "s/\(\.js?\)\(.*\)\(\"><\/script>\)/\1$INPUTVER\3/g" ./srmap/srmap.html
